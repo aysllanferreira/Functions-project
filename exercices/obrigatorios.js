d4f5824feb227b8f9a6deac01609072edf731875
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-11-30 20:49:07 -0300

    Requisito 07 - feito

--- exercices/obrigatorios.js
+++ exercices/obrigatorios.js
@@ -43,12 +43,55 @@ function getPoints(wins, ties, losses) {
 
 function sumMinMax(numbers) {
   // escreva seu codigo aqui
+  return Math.min(...numbers) + Math.max(...numbers);
 }
 
 // Requisito 07
 
 function polarEncoder(sentence) {
   // escreva seu codigo aqui
+  const sentenceArray = sentence.toLowerCase().split('');
+  const newArray = [];
+
+  for (let index = 0; index < sentenceArray.length; index += 1) {
+    switch (sentenceArray[index]) {
+      case 'z':
+        newArray.push('p');
+        break;
+      case 'e':
+        newArray.push('o');
+        break;
+      case 'n':
+        newArray.push('l');
+        break;
+      case 'i':
+        newArray.push('a');
+        break;
+      case 't':
+        newArray.push('r');
+        break;
+      case 'p':
+        newArray.push('z');
+        break;
+      case 'o':
+        newArray.push('e');
+        break;
+      case 'l':
+        newArray.push('n');
+        break;
+      case 'a':
+        newArray.push('i');
+        break;
+      case 'r':
+        newArray.push('t');
+        break;
+      default:
+        newArray.push(sentenceArray[index]);
+        break;
+    }
+  }
+
+  return newArray.join('');
 }
 
 // Requisito 08
