32452251603cf0045942d78b506dbc4ab3b3ea8c
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-11-30 20:04:31 -0300

    atualiza exercicios

--- exercices/osbonus.js
+++ exercices/osbonus.js
@@ -1,27 +1,16 @@
-// Crie um programa que receba um CPF como parâmetro e retorne se ele é válido ou não.
-// Lembre-se que o argumento passado sera testado no seguinte formato: [1,2,3,4,3,2,1,3,2,3,4]
-// O retorno deve ser nesse formato: "CPF: XXX.XXX.XXX-XX" (Onde X é um número).
-// Caso o CPF seja inválido, retorne "CPF Inválido".
+// Requisito Bonus 01
 
 function validateCPF(cpf) {
   // escreva seu codigo aqui
 }
 
-// Crie um programa que seja capaz de fazer o reajuste salarial de um funcionário.
-// O programa deve receber o salário atual do funcionário e o reajuste em porcentagem.
-// Anualmente o funcionário recebe um reajuste anual fixo de 5%.
-// O programa deve retornar o salário atual, o reajuste e o novo salário em 5 anos.
-// Exemplo de retorno: "Novo salário em 5 anos: R$ 1234.56"
+// Requisito Bonus 02
 
 function salaryAdjustment(salary) {
   // escreva seu codigo aqui
 }
 
-// Crie um programa que calcule quantas vezes se repete o maior número em um array.
-// Exemplo: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] => 4
-
-// Crie um programa que receba um array de números e retorne o menor número.
-// Exemplo: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] => 1
+// Requisito Bonus 03
 
 function highestNumber(array) {
   // escreva seu codigo aqui
