c05fe16e5db3fc69e6f2daeead96c629d20bb250
GabrielCardiano <gabriel.cardiano1@gmail.com>
2022-12-05 05:24:09 -0300

    exercícios 4-5-6-7 feitos

--- exercices/obrigatorios.js
+++ exercices/obrigatorios.js
@@ -9,85 +9,92 @@ function sameValues(val1, val2) {
   }
 }
 
-  // Requisito 02
+// Requisito 02
 
-  function convertToCm(meters) {
-    // escreva seu codigo aqui
-    return meters * 100;
-  }
+function convertToCm(meters) {
+  // escreva seu codigo aqui
+  return meters * 100;
+}
 
-  // Requisito 03
+// Requisito 03
 
-  function dobroTriploRaiz(numero) {
-    // escreva seu codigo aqui
-    let dobro = numero * 2;
-    let triplo = numero * 3;
-    let raiz = Math.sqrt(numero);
+function dobroTriploRaiz(numero) {
+  // escreva seu codigo aqui
+  let dobro = numero * 2;
+  let triplo = numero * 3;
+  let raiz = Math.sqrt(numero);
 
-    return `O dobro de ${numero} é ${dobro}, o triplo é ${triplo} e a raiz quadrada é ${raiz}`;
-  }
+  return `O dobro de ${numero} é ${dobro}, o triplo é ${triplo} e a raiz quadrada é ${raiz}`;
+}
+console.log(dobroTriploRaiz(2));
 
-  // Requisito 04
+// Requisito 04
 
-  function createEmail(fullName) {
-    // escreva seu codigo aqui
-  }
+function createEmail(fullName) {
+  // escreva seu codigo aqui
+  return `${fullName.replace(' ', '_').toLowerCase()}@aulao.com`;
 
-  // Requisito 05
+}
+console.log(createEmail('Gabriel Cardiano'));
 
-  function getPoints(wins, ties) {
-    // escreva seu codigo aqui
-  }
+// Requisito 05
 
-  // Requisito 06
+function getPoints(wins, ties) {
+  // escreva seu codigo aqui
+  return (wins * 7) + (ties * 3)
+}
+// Requisito 06
 
-  function sumMinMax(numbers) {
-    // escreva seu codigo aqui
-  }
+function sumMinMax(numbers) {
+  // escreva seu codigo aqui
+  return Math.max(...numbers) + Math.min(...numbers)
+}
 
-  // Requisito 07
+// Requisito 07
 
-  function polarEncoder(sentence) {
-    // escreva seu codigo aqui
-  }
+function polarEncoder(sentence) {
+  // escreva seu codigo aqui
+  return sentence.toLowerCase().replace('z', 'p').replace('e', 'o').replace('n', 'l').replace('i', 'a').replace('t', 'r');
+}
+console.log(polarEncoder('ZeniT'));
 
-  // Requisito 08
+// Requisito 08
 
-  function encodeLetter(letter) {
-    // escreva seu codigo aqui
-  }
+function encodeLetter(letter) {
+  // escreva seu codigo aqui
+}
 
-  function decodeLetter(letter) {
-    // escreva seu codigo aqui
-  }
+function decodeLetter(letter) {
+  // escreva seu codigo aqui
+}
 
-  // Requisito 09
+// Requisito 09
 
-  function sumAndMultiplyBy2(numbers) {
-    // escreva seu codigo aqui
-  }
+function sumAndMultiplyBy2(numbers) {
+  // escreva seu codigo aqui
+}
 
-  // Requisito 10
+// Requisito 10
 
-  function sumEvenNumbers(numbers) {
-    // escreva seu codigo aqui
-  }
+function sumEvenNumbers(numbers) {
+  // escreva seu codigo aqui
+}
 
-  function sumOddNumbers(numbers) {
-    // escreva seu codigo aqui
-  }
+function sumOddNumbers(numbers) {
+  // escreva seu codigo aqui
+}
 
-  module.exports = {
-    sameValues,
-    convertToCm,
-    dobroTriploRaiz,
-    createEmail,
-    getPoints,
-    sumMinMax,
-    polarEncoder,
-    encodeLetter,
-    decodeLetter,
-    sumAndMultiplyBy2,
-    sumEvenNumbers,
-    sumOddNumbers,
-  };
+module.exports = {
+  sameValues,
+  convertToCm,
+  dobroTriploRaiz,
+  createEmail,
+  getPoints,
+  sumMinMax,
+  polarEncoder,
+  encodeLetter,
+  decodeLetter,
+  sumAndMultiplyBy2,
+  sumEvenNumbers,
+  sumOddNumbers,
+};
