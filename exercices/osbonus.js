b9b71be1484bbeb00761d71fa1e9d9b1836f9291
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-05 16:04:36 -0300

    resolvendo requisito bônus 1

--- exercices/osbonus.js
+++ exercices/osbonus.js
@@ -2,6 +2,28 @@
 
 function validateCPF(cpf) {
   // escreva seu codigo aqui
+  for(let index = 0; index < cpf.length; index +=1){
+    if(typeof cpf[index] !== 'number'){
+      return `CPF Inválido`
+    }
+  }
+
+  for(let index = 0; index < cpf.length; index +=1){
+    if(cpf[index] > 9){
+      return `CPF Inválido`
+    }
+  }
+
+  for(let index = 0; index < cpf.length; index +=1){
+    if(cpf[index] < 0){
+      return `CPF Inválido`
+    }
+  }
+const blockOne = cpf.slice(0, 3);
+const blockTwo = cpf.slice(3, 6);
+const blockThree = cpf.slice(6, 9);
+const blockFour = cpf.slice(9, 11);
+  return `CPF: ${blockOne.join('')}.${blockTwo.join('')}.${blockThree.join('')}-${blockFour.join('')}`
 }
 
 // Requisito Bonus 02
