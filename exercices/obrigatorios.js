3b57ace384b58901d637be4f3015a4dea6ba4427
RodrigoRocS <rrsilva1701@gmail.com>
2022-12-04 21:37:51 -0300

    requisito 7

--- exercices/obrigatorios.js
+++ exercices/obrigatorios.js
@@ -48,7 +48,36 @@ function sumMinMax(numbers) {
 // Requisito 07
 
 function polarEncoder(sentence) {
-  // escreva seu codigo aqui
+  const sentLow = sentence.toLowerCase();
+  const arrayZenit = sentLow.split('');
+  let zenitCod = '';
+
+  for (let index = 0; index < arrayZenit.length; index += 1) {
+    if (arrayZenit[index] === 'z') {
+      zenitCod += 'p';
+    } else if (arrayZenit[index] === 'e') {
+      zenitCod += 'o';
+    } else if (arrayZenit[index] === 'n') {
+      zenitCod += 'l';
+    } else if (arrayZenit[index] === 'i') {
+      zenitCod += 'a';
+    } else if (arrayZenit[index] === 't') {
+      zenitCod += 'r';
+    } else if (arrayZenit[index] === 'p') {
+      zenitCod += 'z';
+    } else if (arrayZenit[index] === 'o') {
+      zenitCod += 'e';
+    } else if (arrayZenit[index] === 'l') {
+      zenitCod += 'n';
+    } else if (arrayZenit[index] === 'a') {
+      zenitCod += 'i';
+    } else if (arrayZenit[index] === 'r') {
+      zenitCod += 't';
+    } else {
+      zenitCod += arrayZenit[index];
+    }
+  }
+  return zenitCod;
 }
 
 // Requisito 08
--- teste.js
+++ teste.js
@@ -1,5 +1,34 @@
-function createEmail(fullName) {
-  let mail = fullName.split(" ").join("_");
-  return `${mail}@aulao.com`;
+function polarEncoder(sentence) {
+  const sentLow = sentence.toLowerCase();
+  const arrayZenit = sentLow.split('');
+  let zenitCod = '';
+
+  for (let index = 0; index < arrayZenit.length; index += 1) {
+    if (arrayZenit[index] === 'z') {
+      zenitCod += 'p';
+    } else if (arrayZenit[index] === 'e') {
+      zenitCod += 'o';
+    } else if (arrayZenit[index] === 'n') {
+      zenitCod += 'l';
+    } else if (arrayZenit[index] === 'i') {
+      zenitCod += 'a';
+    } else if (arrayZenit[index] === 't') {
+      zenitCod += 'r';
+    } else if (arrayZenit[index] === 'p') {
+      zenitCod += 'z';
+    } else if (arrayZenit[index] === 'o') {
+      zenitCod += 'e';
+    } else if (arrayZenit[index] === 'l') {
+      zenitCod += 'n';
+    } else if (arrayZenit[index] === 'a') {
+      zenitCod += 'i';
+    } else if (arrayZenit[index] === 'r') {
+      zenitCod += 't';
+    } else {
+      zenitCod += arrayZenit[index];
+    }
+  }
+  return zenitCod;
 }
-console.log(createEmail('rodrigo rocha'));
+
+console.log(polarEncoder('vamos para o hexa brasil'));
