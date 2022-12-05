d069dd6b9c711bcfeadc77e3a903b3425c47ceea
Raionara <raionara.dantas@gmail.com>
2022-12-05 20:30:39 -0300

    até o 6

--- exercices/obrigatorios.js
+++ exercices/obrigatorios.js
@@ -1,18 +1,18 @@
 // Requisito 01
 
-// const sameValues = (val1, val2) => (val1 >= 6, val2 < 6);
-// console.log(sameValues(6,2));
+const sameValues = (val1, val2) => (val1 >= 6, val2 < 6);
+console.log(sameValues(6,2));
 
 // Requisito 02
 
-// const convertToCm = (meter) => (meter *= 100);
-// console.log(convertToCm(1));
+const convertToCm = (meter) => (meter *= 100);
+console.log(convertToCm(1));
 
 // Requisito 03
 
-// const dobroTriploRaiz = (numero) => (`O dobro de ${numero}  é ${numero * 2}, o triplo é ${numero *3} e a raiz quadrada é ${Math.sqrt(numero)}.`);
+const dobroTriploRaiz = (numero) => (`O dobro de ${numero}  é ${numero * 2}, o triplo é ${numero *3} e a raiz quadrada é ${Math.sqrt(numero)}.`);
 
-// console.log(dobroTriploRaiz(4));
+console.log(dobroTriploRaiz(4));
  
 
 // Requisito 04
@@ -23,47 +23,51 @@
 
 // Requisito 05
 
-function getPoints(wins, ties) {
-  // escreva seu codigo aqui
-}
+const getPoints = (wins, ties) => (wins * 7) + (ties * 3);
+
+console.log(((2) * 7) + ((2) * 3));
 
 // // Requisito 06
 
-// function sumMinMax(numbers) {
-//   // escreva seu codigo aqui
-// }
+const sumMinMax = (numbers) => Math.max(...numbers) + (Math.min(...numbers));
+console.log(sumMinMax([3, 2, 5, 4, 7]));
 
-// // Requisito 07
+// Requisito 07
+// A partir da linha 40, faz um console.log e, no code, cria um array. Por isso faz um um for
 
-// function polarEncoder(sentence) {
-//   // escreva seu codigo aqui
-// }
+// const polarEncoder = (sentence) => sentence.toLowerCase().split('');
 
-// // Requisito 08
+//   for (let index = 0; index < sentence.length; index += 1) {
+//     console.log(sentence[index]);
+//   }
 
-// function encodeLetter(letter) {
-//   // escreva seu codigo aqui
-// }
+// console.log(polarEncoder('Brasil, hexa!'));
 
-// function decodeLetter(letter) {
-//   // escreva seu codigo aqui
-// }
+// Requisito 08
 
-// // Requisito 09
+function encodeLetter(letter) {
+  // escreva seu codigo aqui
+}
 
-// function sumAndMultiplyBy2(numbers) {
-//   // escreva seu codigo aqui
-// }
+function decodeLetter(letter) {
+  // escreva seu codigo aqui
+}
 
-// // Requisito 10
+// Requisito 09
 
-// function sumEvenNumbers(numbers) {
-//   // escreva seu codigo aqui
-// }
+function sumAndMultiplyBy2(numbers) {
+  // escreva seu codigo aqui
+}
 
-// function sumOddNumbers(numbers) {
-//   // escreva seu codigo aqui
-// }
+// Requisito 10
+
+function sumEvenNumbers(numbers) {
+  // escreva seu codigo aqui
+}
+
+function sumOddNumbers(numbers) {
+  // escreva seu codigo aqui
+}
 
 module.exports = {
   sameValues,
